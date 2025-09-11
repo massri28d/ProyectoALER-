87acd85b342a76ef887441f4c9ed5ee5b566b2f4
Dago2030 <dieg.alvarado@duocuc.cl>
2025-09-11 18:06:14 -0300

    Menu alergias

--- src/app/pages/menuingredientes/menuingredientes.page.ts
+++ src/app/pages/menuingredientes/menuingredientes.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList, IonItem, IonLabel, IonCheckbox } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -18,14 +18,37 @@ import { RouterModule } from '@angular/router';
     IonTitle,
     IonToolbar,
     IonButton,
-    IonIcon  
+    IonIcon,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonCheckbox
   ]
 })
 export class MenuingredientesPage implements OnInit {
+
+  listaAlergias = [
+    { nombre: 'Maní', seleccionado: false },
+    { nombre: 'Gluten', seleccionado: false },
+    { nombre: 'Lácteos', seleccionado: false },
+    { nombre: 'Mariscos', seleccionado: false },
+    { nombre: 'Huevo', seleccionado: false },
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  guardarSeleccion() {
+    const seleccionadas = this.listaAlergias.filter(alergia => alergia.seleccionado);
+    const nombres = seleccionadas.map(a => a.nombre).join(', ');
+
+    alert(`Seleccionaste: ${nombres}`);
+    // Aquí puedes almacenar en localStorage, enviar a un backend, etc.
+  }
 }
